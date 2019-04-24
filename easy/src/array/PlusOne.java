e6cf4d62e83faedfdd13a39405492021b813d4bb
eizmoo <lajijson@gmail.com>
2019-04-25 00:35:05 +0800

    [SingleNumber仅出现一次的数字] [PlusOne加一 TODO]

---
+++ easy/src/array/PlusOne.java
@@ -0,0 +1,25 @@
+package array;
+
+public class PlusOne {
+
+    /**
+     * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+     *
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+     *
+     * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+     */
+
+    public int[] plusOne(int[] digits) {
+        int length = digits.length;
+
+        int lengthTemp = length - 1;
+
+        int last = digits[lengthTemp];
+
+        while (last == 9) {
+
+        }
+    }
+
+}
---
+++ easy/src/array/SignleNumber.java
@@ -0,0 +1,22 @@
+package array;
+
+public class SignleNumber {
+
+    /**
+     * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+     */
+
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2, 3, 3};
+        System.out.println(new SignleNumber().singleNumber(nums));
+    }
+
+}
