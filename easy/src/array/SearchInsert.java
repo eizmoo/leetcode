db1b3bb6f59041dbf78a59675ff8a90051d0204a
liuwei <lajijson@gmail>
2019-06-13 15:05:25 +0800

    [27.searchInsert 搜索插入位置]

---
+++ easy/src/array/SearchInsert.java
@@ -0,0 +1,21 @@
+package array;
+
+/**
+ * 搜索插入位置
+ */
+public class SearchInsert {
+    public int searchInsert(int[] nums, int target) {
+        int begin = 0, end = nums.length - 1;
+        while (begin <= end) {
+            int mid = (begin + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                begin = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return begin;
+    }
+}
